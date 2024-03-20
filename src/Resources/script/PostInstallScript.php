2c706442bc75a9b63f6d361cd1c8f6be9882b545
Xorg <xorg@neonatis.com>
2024-03-20 23:51:03 +0100

    Adding config\packages files merge/add automatique

--- src/Resources/script/PostInstallScript.php
+++ src/Resources/script/PostInstallScript.php
@@ -3,40 +3,55 @@
     namespace DoctrineEncryptor\DoctrineEncryptorBundle\Resources\script;
 
     use Symfony\Component\Yaml\Yaml;
-
+    require_once dirname(__DIR__, 6) . '/vendor/autoload.php';
+    
     class PostInstallScript
     {
         public static function doctrineEncryptor()
         {
             // Chemin vers le fichier de configuration YAML de votre bundle pour Doctrine Encryptor
-            $source     = dirname(__DIR__, 6). '/config/packages/doctrine_encryptor.yaml';
-            $targetFile = dirname(__DIR__, 1). '/config/setup/doctrine_encryptor.yaml';
-            
-            if (!file_exists($source)) {
-                $config = Yaml::parseFile($targetFile);
-                file_put_contents($source, Yaml::dump($defaultConfig));
-            } else {
-                $existingConfig = Yaml::parseFile($source);
-                $mergedConfig   = array_merge_recursive($targetFile, $existingConfig);
-                file_put_contents($source, Yaml::dump($mergedConfig));
-            }
+            $source = dirname(__DIR__, 6) . '/config/packages/doctrine_encryptor.yaml';
+            $targetFile = dirname(__DIR__, 1) . '/config/setup/doctrine_encryptor.yaml';
+
+            // Charger les configurations existantes
+            $targetConfig = Yaml::parseFile($targetFile);
+            $sourceConfig = file_exists($source) ? Yaml::parseFile($source) : [];
+
+            // Fusionner les configurations
+            $mergedConfig = self::recursiveMerge($sourceConfig, $targetConfig);
+
+            file_put_contents($source, Yaml::dump($mergedConfig, 4));
+
         }
 
         public static function gaufrette()
         {
-            // Chemin vers le fichier de configuration YAML de votre bundle pour Doctrine Encryptor
-            $source     = dirname(__DIR__, 6) . '/config/packages/gaufrette.yaml';
+            $source = dirname(__DIR__, 6) . '/config/packages/gaufrette.yaml';
             $targetFile = dirname(__DIR__, 1) . '/config/setup/gaufrette.yaml';
 
-            if (!file_exists($source)) {
-                $config = Yaml::parseFile($targetFile);
-                file_put_contents($source, Yaml::dump($defaultConfig));
-            } else {
+            // Charger les configurations existantes
+            $targetConfig = Yaml::parseFile($targetFile);
+            $sourceConfig = file_exists($source) ? Yaml::parseFile($source) : [];
 
-                $existingConfig = Yaml::parseFile($source);
-                $mergedConfig   = array_merge_recursive($targetFile, $existingConfig);
-                file_put_contents($source, Yaml::dump($mergedConfig));
+            // Fusionner les configurations
+            $mergedConfig = self::recursiveMerge($sourceConfig, $targetConfig);
+
+            // Écrire la configuration fusionnée dans le fichier de destination
+            file_put_contents($source, Yaml::dump($mergedConfig, 4));
+        }
+        private static function recursiveMerge(array $source, array $target): array
+        {
+            foreach ($source as $key => $value) {
+                if (is_array($value) && isset($target[$key]) && is_array($target[$key])) {
+                    // Si la clé existe dans les deux configurations et que les deux valeurs sont des tableaux,
+                    // fusionner récursivement les sous-tableaux
+                    $target[$key] = self::recursiveMerge($value, $target[$key]);
+                } elseif (!isset($target[$key])) {
+                    // Si la clé n'existe pas déjà dans la configuration cible, l'ajouter
+                    $target[$key] = $value;
+                }
             }
 
+            return $target;
         }
     }
